dbb8b7c1cda129aced32ae9453edf2a3087feb18
Doruk Senel <seneldoruk@gmail.com>
2025-02-06 21:01:23 +0100

    deploy script

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
+  site: "https://seneldoruk.github.io",
+  base: "entgeltatlas-scrape",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
